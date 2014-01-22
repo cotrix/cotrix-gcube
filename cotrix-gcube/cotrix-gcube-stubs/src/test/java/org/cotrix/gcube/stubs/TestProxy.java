44eb05860a1cc64fe780f03bd42c56b3c056a9ea
Federico De Faveri <federico.defaveri@fao.org>
2014-01-22 13:13:31 +0100

    updated test

--- cotrix-gcube/cotrix-gcube-stubs/src/test/java/org/cotrix/gcube/stubs/TestProxy.java
+++ cotrix-gcube/cotrix-gcube-stubs/src/test/java/org/cotrix/gcube/stubs/TestProxy.java
@@ -21,7 +21,7 @@ public class TestProxy {
 	 */
 	public static void main(String[] args) {
 		ServiceStub stub = new ServiceStub();
-		GCubeSession userSession = stub.getSession(new Token("00A39CA7BD7CB6CC6B6649915763B29D", "/gcube/devesec", ""));
+		GCubeSession userSession = stub.getSession(new Token("CAD90443A31AD9E98AF1BEBB35536B1E","/gcube/devsec/devVRE","https://dev.d4science.org:443"));
 		System.out.println(userSession);
 
 	}
