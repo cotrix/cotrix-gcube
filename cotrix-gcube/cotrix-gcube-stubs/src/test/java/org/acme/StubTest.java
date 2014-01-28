c10f460f617760d0d79aebe5fa6e7b77448963cb
fabiosimeoni <fabio.simeoni@me.com>
2014-01-28 11:56:30 +0100

    deals with empty role sets

--- cotrix-gcube/cotrix-gcube-stubs/src/test/java/org/acme/StubTest.java
+++ cotrix-gcube/cotrix-gcube-stubs/src/test/java/org/acme/StubTest.java
@@ -4,6 +4,8 @@ import static java.util.Arrays.*;
 import static org.cotrix.gcube.stubs.SessionContext.*;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.cotrix.gcube.stubs.PortalSession;
 import org.cotrix.gcube.stubs.PortalUser;
 import org.junit.Test;
@@ -15,6 +17,16 @@ public class StubTest {
 	public void roundtrip() {
 		
 		PortalUser user = new PortalUser("n", "fn", "em", asList("r1","r2"));
+		
+		PortalSession session = new PortalSession(user, "s");
+		
+		assertEquals(session,deserialize(serialize(session)));
+	}
+
+	@Test
+	public void noroles() {
+		
+		PortalUser user = new PortalUser("n", "fn", "em", new ArrayList<String>());
 		
 		PortalSession session = new PortalSession(user, "s");
 		
