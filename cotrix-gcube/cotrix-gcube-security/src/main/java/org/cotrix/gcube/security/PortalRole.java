beb13985704336b9cd696ff1fd2f96d745a5729e
Federico De Faveri <federico.defaveri@fao.org>
2014-01-28 12:35:46 +0100

    mapped VRE-DESIGNER role

--- cotrix-gcube/cotrix-gcube-security/src/main/java/org/cotrix/gcube/security/PortalRole.java
+++ cotrix-gcube/cotrix-gcube-security/src/main/java/org/cotrix/gcube/security/PortalRole.java
@@ -5,8 +5,9 @@ import org.cotrix.domain.user.Role;
 
 public enum PortalRole {
 
+	VRE_Designer("VRE-Designer", Roles.MANAGER),
 	VRE_MANAGER("VRE-Manager",Roles.MANAGER),
-	VO_ADMIN("VO-Admin",Roles.ROOT);
+	VO_ADMIN("VO-Admin",Roles.MANAGER);
 	
 	public final String value;
 	public final Role internal;
